d68851e9c89ba1da946ca6bfa2a403cf3ad9a7dd
Frank Castle <attentivemaptrout@protonmail.com>
2019-04-21 20:15:57 +0300

    Code style improvements. Enforced '#' algebra.

--- work.pl
+++ work.pl
@@ -1,71 +1,87 @@
+% For declarative arithmetic.
+:- use_module(library(clpfd)).
+
 and(A, B) :- A, B.
 or(A, B) :- A; B.
 nand(A, B) :- not((and(A,B))).
 nor(A, B) :- not((or(A,B))).
 xor(A, B) :- or(A,B), nand(A,B).
-if(A, B) :- A -> B.
-iff(A, B) :- and(A -> B, B -> A).
-less(A, B) :- A < B.
 
 add(X, L, [X|L]). % push_front
 
-member(X, [X|_]).
-member(X, [_|T]) :- member(X, T).
+member([E|_], E).
+member([_|T], E) :- member(T, E).
+
+len([], N) :- N #= 0.
+len([_|T], N) :- 
+		N #= M + 1, 
+		len(T, M).
+
+min([H], H).
+min([H|T], E) :-
+		min(T, E),
+		H #> E.
+min([H|T], H) :-
+		min(T, E),
+		H #=< E.
 
 append([], B, B).
 append([H|T], B, [H|R]) :- append(T, B, R).
 
 % All L where X has been added somewhere.
-insert(X, L, NL) :- append(A, B, L), append(A, [X|B], NL).
+insert(X, L, NL) :- 
+		append(A, B, L), 
+		append(A, [X|B], NL).
 % [.....][......]  A + B
 % [.....X.......]  NL = A + X + B
 
 % All L where X has been removed.
-remove(X, L, NL) :- append(A, [X|B], L), append(A, B, NL).
+remove(X, L, NL) :- 
+		append(A, [X|B], L), 
+		append(A, B, NL).
 
-%prefix([], _).
-%prefix([H|T], [H|T1]) :- prefix(T, T1).
-prefix(P, L) :- append(P, _, L).
+prefix(L, P) :- append(P, _, L).
 % [....] + [.....] P + _
 % [..............] L
 
-%suffix(L, L).
-%suffix(L, [_|L1]) :- suffix(L, L1).
-suffix(S, L) :- append(_, S, L).
+suffix(L, S) :- append(_, S, L).
 % [....] + [.....] _ + S
 % [..............] L
 
-infix(Sub, L) :- suffix(S, L), prefix(Sub, S).
+infix(L, I) :-
+		suffix(L, S),
+		prefix(S, I).
 % [..............] L
 %         [......] S
 %         [...]    Sub
 
-len([], 0).
-len([_|T], N) :- len(T, M), N is M + 1.
-
-permutation([], []).
-permutation([H|T], P) :- permutation(T, PT), insert(H, PT, P).
+permute([], []).
+permute([H|T], P) :- 
+		permute(T, PT), 
+		insert(H, PT, P).
 % [] -> []
 % [a] -> [a]
 % [b, a] -> [b, a]; [a, b]
 % [a, b, c] -> [a, b, c]....
 
-is_sorted(L) :- not(( append(_, [A, B | _], L), A > B )).
-
+reverse(L, RL) :-
+		reverse_rec(L, [], RL).
 reverse_rec([], S, S).
 reverse_rec([H|T], S, RL) :- 
-	reverse_rec(T, [H|S], RL).
-reverse(L, RL) :-
-	reverse_rec(L, [], RL).
+		reverse_rec(T, [H|S], RL).
 
-min(M, [M]).
-min(M, L) :- 
-	member(M, L),
-	not((member(X, L), less(X, M))).
+% List, number, element.
+nth_element([H|_], 0, H).
+nth_element([_|T], N, E) :-
+		N #= M + 1,
+		nth_element(T, M, E).
 
-nthElement(X, 0, [X|_]).
-nthElement(X, N, [_|T]) :-
-	nthElement(X, M, T),
-	N is M + 1.
+is_sorted([]).
+is_sorted([_]).
+is_sorted([H1, H2|T]) :- 
+		H1 =< H2,
+		is_sorted([H2|T]).
 
-
+bogo_sort(L, SL) :-
+		permute(L, SL),
+		is_sorted(SL).
